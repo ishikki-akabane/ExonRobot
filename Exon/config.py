508846174d184adb3c69040aaa82355ee55a3d8c
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-20 14:59:43 +0530

    Update config.py

--- Exon/config.py
+++ Exon/config.py
@@ -45,19 +45,19 @@ class Config(object):
     ARQ_API_KEY = "TENRCY-KDKSK-MSMSM-OXQYYO-ARQ"
     BOT_ID = "5408158735"
     TOKEN = "5458182410:KINGABISHNOI-UM"
-    OWNER_ID = "5030730429"
+    OWNER_ID = "5673766027" 
     OPENWEATHERMAP_ID = "22322"
-    OWNER_USERNAME = "Abishnoi1M"
-    BOT_USERNAME = "Exon_Robot"
-    SUPPORT_CHAT = "AbishnoiMF"
-    UPDATES_CHANNEL = "Abishnoi_bots"
-    SUPPORT_CHANNEL = "Abishnoi_bots"
-    JOIN_LOGGER = "-1001497222182"
-    EVENT_LOGS = "-1001497222182"
-    ERROR_LOGS = "-1001497222182"
+    OWNER_USERNAME = "SHURYANSH"
+    BOT_USERNAME = "ShizukaXDbot"
+    SUPPORT_CHAT = "team_rocket_bot_suppot"
+    UPDATES_CHANNEL = "Updatesxd"
+    SUPPORT_CHANNEL = "updatesXD"
+    JOIN_LOGGER = "-1001922924926"
+    EVENT_LOGS = "-1001922924926"
+    ERROR_LOGS = "-1001922924926"
 
-    SQLALCHEMY_DATABASE_URI = "postgres://qszfsijv:y6sYqkEb8Z9lFGBmriG7AYjbSbgAJBVk@peanut.db.elephantsql.com/qszfsijv"  # sql
-    DATABASH_URL = "postgres://qszfsijv:y6sYqkEb8Z9lFGBmriG7AYjbSbgAJBVk@peanut.db.elephantsql.com/qszfsijv"  # sql
+    SQLALCHEMY_DATABASE_URI = "postgres://ishikki:FtY1vpm1x9CrHTUHghF5mXDzwv9wshPM@dpg-cgt6brl269vbmet0809g-a.oregon-postgres.render.com/shizuka"  # sql
+    DATABASH_URL = "postgres://ishikki:FtY1vpm1x9CrHTUHghF5mXDzwv9wshPM@dpg-cgt6brl269vbmet0809g-a.oregon-postgres.render.com/shizuka"  # sql
     DB_URL = "postgres://qszfsijv:y6sYqkEb8Z9lFGBmriG7AYjbSbgAJBVk@peanut.db.elephantsql.com/qszfsijvxxqszfsijvx"
     MONGO_DB_URL = "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"  # needed for any database modules
     MONGO_URL = "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"
@@ -65,14 +65,13 @@ class Config(object):
     ARQ_API_URL = "https://arq.hamker.dev"
     BOT_API_URL = "https://api.telegram.org/bot"
     LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    NO_LOAD = ["rss"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""
-    SPAMWATCH_SUPPORT_CHAT = "@AbishnoiMF"
+    SPAMWATCH_SUPPORT_CHAT = "@devslab"
 
-    REDIS_URL = ""
+    REDIS_URL = "redis://Madharjoot:GuKhao123_@redis-12276.c275.us-east-1-4.ec2.cloud.redislabs.com:12276/Madharjoot"
 
     DRAGONS = get_user_list("elevated_users.json", "sudos")
     DEV_USERS = get_user_list("elevated_users.json", "devs")
@@ -82,7 +81,7 @@ class Config(object):
     TIGERS = get_user_list("elevated_users.json", "tigers")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
 
-    DONATION_LINK = "https://t.me/Abishnoi1M"
+    DONATION_LINK = "https://t.me/ishikki_akabane"
     CERT_PATH = None
     STRICT_GBAN = "True"
     PORT = ""
@@ -92,12 +91,12 @@ class Config(object):
     BAN_STICKER = ""
     ALLOW_EXCL = True
     CASH_API_KEY = "NAI H BRO"
-    TIME_API_KEY = "ABISHNOI"
+    TIME_API_KEY = "5LB4TAKPEKZ0"
     WALL_API = "F-OFF"
     AI_API_KEY = "LOVEYOU"
     BL_CHATS = []
     SPAMMERS = None
-    SPAMWATCH_API = ""
+    SPAMWATCH_API = "AHT~6SSflemqFZa5tfkKFQeiLnJUwoowWYozC3SN2ZY_aSGh83~9pItAmM4Mi40E"
     ALLOW_CHATS = None
     TEMP_DOWNLOAD_DIRECTORY = "./"
     HEROKU_APP_NAME = ""
@@ -112,8 +111,8 @@ class Config(object):
     HELP_IMG = "https://telegra.ph/file/14d1f98500af1132e5460.jpg"
     START_IMG = "https://telegra.ph/file/14d1f98500af1132e5460.jpg"
     TIME_API_KEY = "5LB4TAKPEKZ0"
-    INFOPIC = False
-    GENIUS_API_TOKEN = "28jwoKAkskaSjsnsksAjnwjUJwj"
+    INFOPIC = True
+    GENIUS_API_TOKEN = "gIgMyTXuwJoY9VCPNwKdb_RUOA_9mCMmRlbrrdODmNvcpslww_2RIbbWOB8YdBW9"
 
 
 class Production(Config):
